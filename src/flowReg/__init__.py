837cc643e0b21f02524fcf692f9f6223238970cc
Ubuntu <ubuntu@mistybonobo-fb145.openstack.bielefeld>
2024-02-14 11:23:07 +0000

    dataset and preprocessing fens

--- src/flowReg/__init__.py
+++ src/flowReg/__init__.py
@@ -1,7 +1,7 @@
 from importlib.metadata import version
 
-from . import pl, pp, tl
+from . import pl, pp, preprocessing, tl, tools
 
-__all__ = ["pl", "pp", "tl"]
+__all__ = ["pl", "pp", "tl", "preprocessing", "tools"]
 
-__version__ = version("flowReg")
+from ._version import __version__
